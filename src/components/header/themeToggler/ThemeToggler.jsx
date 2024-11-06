45a0438aa75ab84ac9898d03162fb793f4cf4944
OneClickTechy <oneclicktechy@gmail.com>
2024-11-06 15:54:58 +0530

    delete unusual state in toggler

--- src/components/header/themeToggler/ThemeToggler.jsx
+++ src/components/header/themeToggler/ThemeToggler.jsx
@@ -3,14 +3,12 @@ import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 const ThemeToggler = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-  const [isOn, setIsOn] = useState(false);
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [theme]);
   const handleToggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
-    setIsOn(theme !== "light" ? true : false);
   };
   return (
     <>
@@ -23,7 +21,7 @@ const ThemeToggler = () => {
             theme === "dark" ? "translate-x-[100%]" : null
           } transition-transform ease-linear duration-300 w-[25px] h-[25px] bg-text-primary rounded-3xl flex justify-center items-center`}
         >
-          {isOn ? (
+          {theme === "dark" ? (
             <MdDarkMode className="text-[black] " />
           ) : (
             <MdLightMode className="text-[white]" />
