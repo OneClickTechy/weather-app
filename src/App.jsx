5a2d0ea314f80d7889ef861cb00e5dc76f02d920
OneClickTechy <oneclicktechy@gmail.com>
2024-11-09 00:11:54 +0530

    remove unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useGeoContext } from "./context/geoContext.jsx";
 import Header from "./components/header/Header.jsx";
 import Main from "./components/main/Main.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
 const App = () => {
-  
-  const { showResults } = useGeoContext();
   return (
     <div className="transition-colors duration-300 ease-in-out bg-background text-text-primary flex flex-col w-full min-h-screen gap-4">
       <Header />
